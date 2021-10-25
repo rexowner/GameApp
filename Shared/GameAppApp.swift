ae78209b3d9127ab3fea73f632d43b73f322f623
Les Poltrack <lapwack@gmail.com>
2021-10-24 22:35:45 -0700

    Initial Commit

---
+++ Shared/GameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameAppApp.swift
+//  Shared
+//
+//  Created by Les Poltrack on 10/24/21.
+//
+
+import SwiftUI
+
+@main
+struct GameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
